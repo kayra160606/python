b1f85ff8a4730ae38717035cf27c599273371912
Mohithacker1 <125952416+Mohithacker1@users.noreply.github.com>
2024-07-05 10:18:09 +0530

    Add files via upload

---
+++ unit1pract5.py
@@ -0,0 +1,20 @@
+a =10
+b =20
+list = [1,2,3,4,5];
+if(a is list):
+    print("Line 1 a is avialable in the given list")
+else:
+    print("Line 1 - a is not avialable in the given list")
+    
+    if(b not in list):
+       print("Line 2 - b is  not avialable in the given list")
+    else:
+       print("Line 2 - b is  avialable in the given list")
+    
+    if(a is list):
+       print("Line 3 - a is avialable in the given list")
+    else:
+       print("Line 3 - a is not avialable in the given list")
+    
+    
+    
