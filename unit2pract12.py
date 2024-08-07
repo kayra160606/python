e056936099d89612d4952001554c33c32f429625
Mohithacker1 <125952416+Mohithacker1@users.noreply.github.com>
2024-08-07 11:07:35 +0530

    Update unit2pract12.py

--- unit2pract12.py
+++ unit2pract12.py
@@ -1,5 +1,12 @@
 def plus_one(number):
     def add_one(number):
-        return number + 3
+        val=number()
+        return val + 3
     result = add_one(number)
     return result
+
+def number():
+    return 10
+
+res=plus_one(number)
+print(res)
